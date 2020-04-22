f5d8cd3dc41bf5749ebaded69662507e82d67e0b
mick-roper <mick@theropers.co.uk>
2020-04-22 10:50:06 +0100

    added common stuff

---
+++ pkg/common/go.mod
@@ -0,0 +1,3 @@
+module github.com/mick-roper/hubl/pkg/common
+
+go 1.14
---
+++ pkg/common/types.go
@@ -0,0 +1,17 @@
+package common
+
+type (
+	// Subscription that describes a topic and a destination
+	Subscription struct {
+		ID             string
+		Topic          string
+		DestinationURL string
+	}
+
+	// SubscriptionStore dols subscription information
+	SubscriptionStore interface {
+		GetSubscriptionsForTopic(topic string) []Topic
+		AddSubscription(Subscription) error
+		DeleteSubscription(id string) error
+	}
+)
