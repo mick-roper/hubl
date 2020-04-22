8f9e4c214bbfc9b437cc977f1dd2967f93b61d3b
mick-roper <mick@theropers.co.uk>
2020-04-22 19:55:47 +0100

    added some erro handling

--- pkg/web/subscription-handler.go
+++ pkg/web/subscription-handler.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/mick-roper/hubl/pkg/common"
@@ -10,6 +11,20 @@ import (
 func NewSubscriptionHandler(store common.SubscriptionStore) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
+		case http.MethodGet:
+			{
+				subs := store.GetAll()
+				bytes, err := json.Marshal(subs)
+
+				if err != nil {
+					w.WriteHeader(500)
+					w.Write([]byte(fmt.SPrintf("{ \"error\": \"%v\" }", err.Error())))
+					return
+				}
+
+				w.WriteHeader(200)
+				w.Write(bytes)
+			}
 		default:
 			{
 				w.WriteHeader(http.StatusMethodNotAllowed)
